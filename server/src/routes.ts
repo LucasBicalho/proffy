932ba357889bada4198b1ee436efc2bde13f9a97
Lucas Bicalho <lucassbcosta@hotmail.com>
2020-08-09 19:33:16 -0300

    refactor(server): add controller to classes route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,59 +1,10 @@
 import express from "express";
-import database from "./database/connection";
-import convertHourToMinutes from "./utils/convertHourToMinutes";
+
+import ClassesController from "./controllers/ClassesController";
 
 const routes = express.Router();
+const classesController = new ClassesController();
 
-interface ScheduleItem {
-  week_day: number;
-  from: string;
-  to: string;
-}
-
-routes.post("/classes", async (request, response) => {
-  const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
-
-  const transaction = await database.transaction();
-
-  try {
-    const insertedUsersIds = await transaction("users").insert({
-      name,
-      avatar,
-      whatsapp,
-      bio,
-    });
-
-    const user_id = insertedUsersIds[0];
-
-    const insertedClassesIds = await transaction("classes").insert({
-      subject,
-      cost,
-      user_id,
-    });
-
-    const class_id = insertedClassesIds[0];
-
-    const classSchedule = schedule.map((scheduleItem: ScheduleItem) => {
-      return {
-        week_day: scheduleItem.week_day,
-        from: convertHourToMinutes(scheduleItem.from),
-        to: convertHourToMinutes(scheduleItem.to),
-        class_id,
-      };
-    });
-
-    await transaction("class_schedule").insert(classSchedule);
-
-    await transaction.commit();
-
-    return response.status(201).send();
-  } catch (error) {
-    await transaction.rollback();
-
-    return response.status(400).json({
-      error: "Unexpected error while creating new class",
-    });
-  }
-});
+routes.post("/classes", classesController.create);
 
 export default routes;
