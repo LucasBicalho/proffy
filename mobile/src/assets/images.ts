f1832fa212454e6d13afae7fe33e5e3a4039a740
Lucas Bicalho <lucassbcosta@hotmail.com>
2020-08-13 01:11:00 -0300

    feat(mobile): add assets

---
+++ mobile/src/assets/images.ts
@@ -0,0 +1,3 @@
+export { default as giveClassesBackgroundImage } from "./images/give-classes-background.png";
+export { default as landingImage } from "./images/landing.png";
+export { default as logoImage } from "./images/logo.png";
---
+++ mobile/src/assets/images/icons.ts
@@ -0,0 +1,7 @@
+export { default as backIcon } from "./icons/back.png";
+export { default as giveClassesIcon } from "./icons/give-classes.png";
+export { default as heartIcon } from "./icons/heart.png";
+export { default as heartOutlineIcon } from "./icons/heart-outline.png";
+export { default as studyIcon } from "./icons/study.png";
+export { default as unfavoriteIcon } from "./icons/unfavorite.png";
+export { default as whatsappIcon } from "./icons/whatsapp.png";
